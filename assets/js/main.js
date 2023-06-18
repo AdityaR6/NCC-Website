dd176db4164beb3b67f233ae5e35a11512852f8c
Aditya Rajauria <78144479+AdityaR6@users.noreply.github.com>
2023-06-18 17:47:58 +0530

    Added links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: OnePage
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
-* Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
